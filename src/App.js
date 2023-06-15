e36d2b8a7adda4fce9c2752e71a5fc10c30dc694
kozsoy <kadirozsoy888@gmail.com>
2023-06-15 23:09:24 +0300

    revize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 function App() {
+  const [veri, SetVeri] = useState();
+  const [tarih, SetTarih] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/ozanerturk/covid19-turkey-api/master/dataset/timeline.json"
+      )
+      .then((res) => SetVeri(res.data[tarih]))
+      .catch((err) => console.log(err));
+  }, [veri, tarih]);
+
   return (
     <div className="App">
       <div className="container">
@@ -12,6 +27,7 @@ function App() {
               type="text"
               placeholder="GG/AA/YY şeklinde giriniz."
               className="form-control text-center mt-4"
+              onChange={(e) => SetTarih(e.target.value)}
             />
             <br />
             <table class="table text-center">
@@ -26,9 +42,11 @@ function App() {
               <tbody>
                 <tr class="fw-semibold text-center ">
                   <th scope="row">1</th>
-                  <td>12500</td>
-                  <td>2200</td>
-                  <td>17</td>
+                  <td>
+                    {veri === undefined ? "Veri Bekleniyor." : veri.totalTests}
+                  </td>
+                  <td>{veri === undefined ? "Veri Bekleniyor." : veri.totalPatients}</td>
+                  <td>{veri === undefined ? "Veri Bekleniyor." : veri.totalDeaths}</td>
                 </tr>
               </tbody>
             </table>
